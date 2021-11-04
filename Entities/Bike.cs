845fca65bad7969d7c0bacbc7de0146f68254ca9
Dmitriy-Volkov18 <anexperienced.programmer@gmail.com>
2021-11-04 22:27:46 +0200

    modify: added model, context and connection to sql server

---
+++ Entities/Bike.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeRental.Entities
+{
+    public enum BikeType
+    {
+        Road,
+        Mountain,
+        Touring,
+        Track,
+        Bmx,
+        Hybrid,
+        Cruiser,
+        Folding,
+        Kids
+    }
+
+    public enum BikeStatus
+    {
+        Free,
+        Rented
+    }
+
+    public class Bike
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public BikeType Type { get; set; }
+        public decimal Price { get; set; }
+        public BikeStatus Status { get; set; }
+    }
+}
